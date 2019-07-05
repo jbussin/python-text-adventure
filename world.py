abe4d738cd744aac67eb193ad9250444724e7e31
Jonathan Bussing <jbussing@ucsd.edu>
2019-07-05 12:32:42 -0700

    fixed map.txt ref in world.py

--- world.py
+++ world.py
@@ -15,7 +15,7 @@ def tile_exists(x, y):
 
 def load_tiles():
     """Parses a file that describes the world space into the _world object"""
-    with open('C:/Users/HyperSentient/Desktop/text-adventure-tut-master/adventuretutorial/resources/map.txt', 'r') as f:
+    with open('resources/map.txt', 'r') as f:
         rows = f.readlines()
     x_max = len(rows[0].split('\t'))
     for y in range(len(rows)):
@@ -27,5 +27,12 @@ def load_tiles():
                 starting_position = (x, y)
                 _world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
 
+with open('resources/map.txt', 'r') as f:
+     rows = f.readlines()
+     x_max = len(rows[0].split('\t'))
+for y in range(len(rows)):
+     cols = rows[y].split('\t')
 
+print(rows)
+print(cols)
 
